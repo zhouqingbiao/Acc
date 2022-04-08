368067e989ccbdc1cfee993e6535fc40c6c932cc
zhouqingbiao <zhouqingbiao@qq.com>
2022-04-08 19:03:10 +0800

    1

--- app/src/main/java/com/zhouqingbiao/acc/MainActivity.kt
+++ app/src/main/java/com/zhouqingbiao/acc/MainActivity.kt
@@ -1,11 +1,28 @@
 package com.zhouqingbiao.acc
 
-import androidx.appcompat.app.AppCompatActivity
+import android.app.ActivityManager
+import android.content.ComponentName
+import android.content.Context
+import android.content.Intent
 import android.os.Bundle
+import android.provider.Settings
+import android.view.View
+import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    fun startXxqg(view: View) {
+        var am = getSystemService(Context.ACTIVITY_SERVICE) as ActivityManager
+        var cn = ComponentName("com.zhouqingbiao.acc", "com.zhouqingbiao.acc.AccService")
+        if (am.getRunningServiceControlPanel(cn) == null) {
+            startActivity(Intent(Settings.ACTION_ACCESSIBILITY_SETTINGS))
+            startActivity(packageManager.getLaunchIntentForPackage("cn.xuexi.android"))
+        } else {
+            startActivity(packageManager.getLaunchIntentForPackage("cn.xuexi.android"))
+        }
     }
 }
