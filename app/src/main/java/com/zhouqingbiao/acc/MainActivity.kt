ee410992d212dbd1dbc3eee6863cdfaee697692f
zhouqingbiao <zhouqingbiao@qq.com>
2022-04-08 19:36:27 +0800

    1

--- app/src/main/java/com/zhouqingbiao/acc/MainActivity.kt
+++ app/src/main/java/com/zhouqingbiao/acc/MainActivity.kt
@@ -7,7 +7,9 @@ import android.content.Intent
 import android.os.Bundle
 import android.provider.Settings
 import android.view.View
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import com.google.android.material.switchmaterial.SwitchMaterial
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -15,14 +17,18 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
     }
 
-    fun startXxqg(view: View) {
+    fun switchAcc(view: View) {
         var am = getSystemService(Context.ACTIVITY_SERVICE) as ActivityManager
         var cn = ComponentName("com.zhouqingbiao.acc", "com.zhouqingbiao.acc.AccService")
         if (am.getRunningServiceControlPanel(cn) == null) {
             startActivity(Intent(Settings.ACTION_ACCESSIBILITY_SETTINGS))
-            startActivity(packageManager.getLaunchIntentForPackage("cn.xuexi.android"))
         } else {
-            startActivity(packageManager.getLaunchIntentForPackage("cn.xuexi.android"))
+            Toast.makeText(this, "无障碍已开启", Toast.LENGTH_SHORT).show()
         }
+        findViewById<SwitchMaterial>(R.id.switch_acc).isChecked = true
+    }
+
+    fun startXxqg(view: View) {
+        startActivity(packageManager.getLaunchIntentForPackage("cn.xuexi.android"))
     }
 }
