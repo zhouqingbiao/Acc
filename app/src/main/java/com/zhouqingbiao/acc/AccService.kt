ef8313ce969337303ca490f5746d5a09de83da45
zhouqingbiao <zhouqingbiao@qq.com>
2022-04-08 17:51:09 +0800

    1

---
+++ app/src/main/java/com/zhouqingbiao/acc/AccService.kt
@@ -0,0 +1,13 @@
+package com.zhouqingbiao.acc
+
+import android.accessibilityservice.AccessibilityService
+import android.view.accessibility.AccessibilityEvent
+
+class AccService : AccessibilityService() {
+    override fun onAccessibilityEvent(p0: AccessibilityEvent?) {
+    }
+
+    override fun onInterrupt() {
+
+    }
+}
