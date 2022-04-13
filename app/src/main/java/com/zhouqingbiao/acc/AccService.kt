406395157a5689e7140473e08f44cb441363c36d
zhouqingbiao <zhouqingbiao@qq.com>
2022-04-13 10:29:43 +0800

    1

--- app/src/main/java/com/zhouqingbiao/acc/AccService.kt
+++ app/src/main/java/com/zhouqingbiao/acc/AccService.kt
@@ -7,6 +7,113 @@ import android.view.accessibility.AccessibilityNodeInfo
 
 class AccService : AccessibilityService() {
 
+    // 登录1分
+    var dlText = "登录"
+    var dlBoolean = false
+    lateinit var dl: AccessibilityNodeInfo
+    lateinit var dlF: AccessibilityNodeInfo
+    lateinit var dlClick: AccessibilityNodeInfo
+
+    // 我要选读文章12分
+    var wyxdwzText = "我要选读文章"
+    var wyxdwzBoolean = false
+    lateinit var wyxdwz: AccessibilityNodeInfo
+    lateinit var wyxdwzF: AccessibilityNodeInfo
+    lateinit var wyxdwzClick: AccessibilityNodeInfo
+
+    // 视听学习6分
+    var stxxText = "视听学习"
+    var stxxBoolean = false
+    lateinit var stxx: AccessibilityNodeInfo
+    lateinit var stxxF: AccessibilityNodeInfo
+    lateinit var stxxClick: AccessibilityNodeInfo
+
+    // 视听学习时长6分
+    var stxxscText = "视听学习时长"
+    var stxxscBoolean = false
+    lateinit var stxxsc: AccessibilityNodeInfo
+    lateinit var stxxscF: AccessibilityNodeInfo
+    lateinit var stxxscClick: AccessibilityNodeInfo
+
+    // 每日答题5分
+    var mrdtText = "每日答题"
+    var mrdtBoolean = false
+    lateinit var mrdt: AccessibilityNodeInfo
+    lateinit var mrdtF: AccessibilityNodeInfo
+    lateinit var mrdtClick: AccessibilityNodeInfo
+
+    // 每周答题5分
+    var mzdtText = "每周答题"
+    var mzdtBoolean = false
+    lateinit var mzdt: AccessibilityNodeInfo
+    lateinit var mzdtF: AccessibilityNodeInfo
+    lateinit var mzdtClick: AccessibilityNodeInfo
+
+    // 专项答题10分
+    var zxdtText = "专项答题"
+    var zxdtBoolean = false
+    lateinit var zxdt: AccessibilityNodeInfo
+    lateinit var zxdtF: AccessibilityNodeInfo
+    lateinit var zxdtClick: AccessibilityNodeInfo
+
+    // 挑战答题6分
+    var tzdtText = "挑战答题"
+    var tzdtBoolean = false
+    lateinit var tzdt: AccessibilityNodeInfo
+    lateinit var tzdtF: AccessibilityNodeInfo
+    lateinit var tzdtClick: AccessibilityNodeInfo
+
+    // 四人赛5分
+    var srsText = "四人赛"
+    var srsBoolean = false
+    lateinit var srs: AccessibilityNodeInfo
+    lateinit var srsF: AccessibilityNodeInfo
+    lateinit var srsClick: AccessibilityNodeInfo
+
+    // 双人对战2分
+    var srdzText = "双人对战"
+    var srdzBoolean = false
+    lateinit var srdz: AccessibilityNodeInfo
+    lateinit var srdzF: AccessibilityNodeInfo
+    lateinit var srdzClick: AccessibilityNodeInfo
+
+    // 订阅2分
+    var dyText = "订阅"
+    var dyBoolean = false
+    lateinit var dy: AccessibilityNodeInfo
+    lateinit var dyF: AccessibilityNodeInfo
+    lateinit var dyClick: AccessibilityNodeInfo
+
+    // 分享1分
+    var fxText = "分型"
+    var fxBoolean = false
+    lateinit var fx: AccessibilityNodeInfo
+    lateinit var fxF: AccessibilityNodeInfo
+    lateinit var fxClick: AccessibilityNodeInfo
+
+    // 发表观点1分
+    var fbgdText = "发表观点"
+    var fbgdBoolean = false
+    lateinit var fbgd: AccessibilityNodeInfo
+    lateinit var fbgdF: AccessibilityNodeInfo
+    lateinit var fbgdClick: AccessibilityNodeInfo
+
+    // 本地频道1分
+    var bdpdText = "本地频道"
+    var bdpdBoolean = false
+    lateinit var bdpd: AccessibilityNodeInfo
+    lateinit var bdpdF: AccessibilityNodeInfo
+    lateinit var bdpdClick: AccessibilityNodeInfo
+
+    // 强国运动2分
+    var qgydText = "强国运动"
+    var qgydBoolean = false
+    lateinit var qgyd: AccessibilityNodeInfo
+    lateinit var qgydF: AccessibilityNodeInfo
+    lateinit var qgydClick: AccessibilityNodeInfo
+
+    // ------------------------------------------------------------
+
     // 学
     var xue = "学"
 
@@ -30,60 +137,18 @@ class AccService : AccessibilityService() {
     private lateinit var gz: AccessibilityNodeInfo
     private var gzCount = 0
 
-    // 登录1分
-    var dl = "登录"
-
-    // 登录积分
-    var dljf = ""
-
-    // 我要选读文章12分
-    var wyxdwz = "我要选读文章"
-
-    // 视听学习6分
-    var stxx = "视听学习"
-
-    // 视听学习时长6分
-    var stxxsc = "视听学习时长"
-
-    // 每日答题5分
-    var mrdt = "每日答题"
-
-    // 每周答题5分
-    var mzdt = "每周答题"
-
-    // 专项答题10分
-    var zxdt = "专项答题"
-
-    // 挑战答题6分
-    var tzdt = "挑战答题"
-
-    // 四人赛5分
-    var srs = "四人赛"
-
-    // 双人对战2分
-    var srdz = "双人对战"
-
-    // 订阅2分
-    var dy = "订阅"
-
-    // 分享1分
-    var fx = false
-
-    // 发表观点1分
-    private var fbgd = false
-
-    // 本地频道1分
-    var bdpd = "本地频道"
-
-    // 强国运动2分
-    var qgyd = "强国运动"
-
 
     private lateinit var menuButton: AccessibilityNodeInfo
     var menuButtonCount = 0
 
     private lateinit var szButton: AccessibilityNodeInfo
     var szButtonCount = 0
+
+    override fun onServiceConnected() {
+        super.onServiceConnected()
+
+
+    }
 
     override fun onAccessibilityEvent(event: AccessibilityEvent?) {
         val text = "强国城兑福利"
@@ -131,20 +196,24 @@ class AccService : AccessibilityService() {
 //            fx = true
 //        }
 
+        if (!dlBoolean and !wyxdwzBoolean and !stxxBoolean and !stxxscBoolean and !mrdtBoolean and !mzdtBoolean and !zxdtBoolean and !tzdtBoolean and !tzdtBoolean and !srsBoolean and !srdzBoolean and !dyBoolean and !fbgdBoolean and !bdpdBoolean and !qgydBoolean) {
+            val accessibilityNodeInfosRootInActiveWindow =
+                rootInActiveWindow.findAccessibilityNodeInfosByViewId(viewId)
+            if (accessibilityNodeInfosRootInActiveWindow.size > 0) {
+                (0 until accessibilityNodeInfosRootInActiveWindow.size).forEach { index ->
+                    Log.i(
+                        xue,
+                        "findByViewId->rootInActiveWindow->${index}->${accessibilityNodeInfosRootInActiveWindow[index].viewIdResourceName}"
+                    )
+                    // 遍历所有子节点
+                    recycle(mutableListOf(accessibilityNodeInfosRootInActiveWindow[index]))
+                    // 查找学习积分标题
+                    findXxjfBt()
+                    // 查找学习积分明细
 
-        val accessibilityNodeInfosRootInActiveWindow =
-            rootInActiveWindow.findAccessibilityNodeInfosByViewId(viewId)
-        if (accessibilityNodeInfosRootInActiveWindow.size > 0) {
-            (0 until accessibilityNodeInfosRootInActiveWindow.size).forEach { index ->
-                Log.i(
-                    xue,
-                    "findByViewId->rootInActiveWindow->${index}->${accessibilityNodeInfosRootInActiveWindow[index].viewIdResourceName}"
-                )
-                recycle(mutableListOf(accessibilityNodeInfosRootInActiveWindow[index]))
-                findByText()
+                }
             }
         }
-
 
         // 本地
         if (bdCount == 0) {
@@ -195,10 +264,54 @@ class AccService : AccessibilityService() {
         }
     }
 
-    private fun findByText() {
+    // 查找学习积分标题
+    private fun findXxjfBt() {
         mutableListAccessibilityNodeInfo.forEach { ani ->
-            Log.i(xue, "${ani.className}")
-            Log.i(xue, "${ani.text}")
+            if (ani.text != null) {
+                when (ani.text) {
+                    dlText -> dl = ani
+                    wyxdwzText -> wyxdwz = ani
+                    stxxText -> stxx = ani
+                    stxxscText -> stxxsc = ani
+                    mrdtText -> mrdt = ani
+                    mzdtText -> mzdt = ani
+                    zxdtText -> zxdt = ani
+                    tzdtText -> tzdt = ani
+                    srsText -> srs = ani
+                    srdzText -> srdz = ani
+                    dyText -> dy = ani
+                    fxText -> fx = ani
+                    fbgdText -> fbgd = ani
+                    bdpdText -> bdpd = ani
+                    qgydText -> qgyd = ani
+                }
+            }
+            if (ani.isClickable) {
+                dlClick = ani
+
+                Log.i(xue, "${ani.className}")
+                Log.i(xue, "${ani.text}")
+            }
+        }
+    }
+
+    // 查找学习积分明细
+    private fun findXxjfMx() {
+        mutableListAccessibilityNodeInfo.forEach { ani ->
+            if (ani.text != null) {
+                if (ani.text.contains("已获")) {
+                    dlF = ani
+
+                    Log.i(xue, "${ani.className}")
+                    Log.i(xue, "${ani.text}")
+                }
+            }
+            if (ani.isClickable) {
+                dlClick = ani
+
+                Log.i(xue, "${ani.className}")
+                Log.i(xue, "${ani.text}")
+            }
         }
     }
 
